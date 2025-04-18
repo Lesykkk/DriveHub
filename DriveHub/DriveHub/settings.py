77a601c832bc1c081e01346e57480277c7e33397
RomanLeskiv <74130593+Lesykkk@users.noreply.github.com>
2025-04-18 22:15:29 +0300

    Fixed migrations bag (#27)
    Fixed migrations bag, set default custom auth model
    
    Co-authored-by: So1larder <206430565+So1larder@users.noreply.github.com>

--- DriveHub/DriveHub/settings.py
+++ DriveHub/DriveHub/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'account.User'
+LOGIN_URL = 'account:login'
+LOGIN_REDIRECT_URL = 'account:profile'
