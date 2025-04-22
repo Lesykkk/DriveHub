82d2d1e0a68dde36d206986c2e070da37220f34a
olex2211 <129803686+olex2211@users.noreply.github.com>
2025-04-22 16:57:16 +0300

    13 frontend create and style profile page (#30)
    * Added side-bar menu
    
    * Added my-adverts section
    
    * Added settings
    
    Co-Authored-By: RomanLeskiv <74130593+Lesykkk@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: RomanLeskiv <2017.roman.8@gmail.com>
    Co-authored-by: RomanLeskiv <74130593+Lesykkk@users.noreply.github.com>

--- DriveHub/account/urls.py
+++ DriveHub/account/urls.py
@@ -6,5 +6,7 @@ app_name = 'account'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('registration/', views.registration, name='registration'),
+    path('my-adverts/', views.my_adverts, name='my-adverts'),
+    path('favourite/', views.favourite, name='favourite'),
+    path('settings/', views.settings, name='settings'),
 ]
-
--- DriveHub/account/views.py
+++ DriveHub/account/views.py
@@ -5,3 +5,12 @@ def login(request):
 
 def registration(request):
     return render(request, 'account/registration.html')
+
+def my_adverts(request):
+    return render(request, 'account/my-adverts.html')
+
+def favourite(request):
+    return render(request, 'account/favourite.html')
+
+def settings(request):
+    return render(request, 'account/settings.html')
