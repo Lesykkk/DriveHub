ff57743789b57beec8bdab3464e04c3c64c9d310
RomanLeskiv <2017.roman.8@gmail.com>
2025-05-05 01:29:43 +0300

    Added styled advert`s page

--- DriveHub/advert/urls.py
+++ DriveHub/advert/urls.py
@@ -6,5 +6,6 @@ app_name = 'advert'
 urlpatterns = [
     path('', views.home, name='home'),
     path('create-advert/', views.create_advert, name='create-advert'),
-    path('ajax/get-models/', views.get_models_by_brand, name='get-models-by-brand')
+    path('ajax/get-models/', views.get_models_by_brand, name='get-models-by-brand'),
+    path('advert-detail/', views.advert_detail, name='advert-detail'),
 ]
